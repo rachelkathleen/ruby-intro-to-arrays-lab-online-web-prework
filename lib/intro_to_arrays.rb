142f1c5d8eff3dcfa7aa6f58fce02056f49a0196
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-06 03:11:26 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,24 @@ end
 def array_with_two_elements
    [1, 2]
 end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
+
+def first_element_with_array_methods
+end
+
+def last_element_with_array_methods
+end
+
+def length_of_array
+end
